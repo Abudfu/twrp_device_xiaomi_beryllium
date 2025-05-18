379414f2d7049b07061672b8e362498af195e7fe
Rve27 <rve27github@gmail.com>
2025-05-19 02:56:15 +0700

    beryllium: Define brightness control path
    Change-Id: Ib09502efaece071b6337906d888cc3658f55ca6c
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,7 @@ TW_INCLUDE_CRYPTO_FBE := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_REPACKTOOLS := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 
 # QCOM Decryption
 BOARD_USES_QCOM_FBE_DECRYPTION := true
