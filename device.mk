74d6e912a8fd227952a2b29e2780dd47d64af176
Rve27 <rve27github@gmail.com>
2025-05-20 02:14:25 +0700

    beryllium: Add necessary libraries
    Change-Id: Ib0c6ddfeaff82aab6ce28d2d5e9f72a142cf108a
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- device.mk
+++ device.mk
@@ -11,3 +11,14 @@ LOCAL_PATH := device/xiaomi/beryllium
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libandroidicu \
+    libion
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
+
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/beryllium/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
